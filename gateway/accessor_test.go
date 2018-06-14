f216b7a0c3aa48021a519367dfc4341bb80dddc5
dylen <dylenfu@126.com>
2018-06-14 12:12:40 +0800

    test cancel/cutoff/cutoffpair

--- gateway/accessor_test.go
+++ gateway/accessor_test.go
@@ -212,7 +212,7 @@ func TestEthNodeAccessor_CancelOrder(t *testing.T) {
 		state        types.OrderState
 		err          error
 		result       string
-		orderhash    = common.HexToHash("0x2baab0193b1cd32529c90bc1cb7416de25d7c2a92cf08f2c20fed184d26173ad")
+		orderhash    = common.HexToHash("0x2fd51638ad98d79aef0e1aecd5d85c5a471914792b9aaf74afdb1fafd5c25ff2")
 		cancelAmount = new(big.Int).Mul(big.NewInt(1e18), big.NewInt(2))
 	)
 
@@ -290,7 +290,7 @@ func TestEthNodeAccessor_GetCutoffAll(t *testing.T) {
 }
 
 func TestEthNodeAccessor_CutoffPair(t *testing.T) {
-	account := common.HexToAddress("0xb1018949b241D76A1AB2094f473E9bEfeAbB5Ead")
+	account := common.HexToAddress("0x1B978a1D302335a6F2Ebe4B8823B5E17c3C84135")
 	cutoff := big.NewInt(1531107175)
 	token1 := lrcTokenAddress
 	token2 := wethTokenAddress
